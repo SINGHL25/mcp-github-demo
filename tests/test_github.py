32d4bf38641b3b5d08cfe8a37cca775e106009f6
Akhilesh singh <akhi.singh1989@gmail.com>
2025-09-05 06:06:15 +1000

    Update test_github.py

--- tests/test_github.py
+++ tests/test_github.py
@@ -1,1 +1,13 @@
 
+import pytest
+from fastapi.testclient import TestClient
+from src.main import app
+
+client = TestClient(app)
+
+def test_get_repositories():
+    response = client.post("/repos", json={"username": "octocat"})
+    assert response.status_code == 200
+    data = response.json()
+    assert isinstance(data, list)
+    assert "name" in data[0]
